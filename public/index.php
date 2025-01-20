ea1acc434e925dc421b1bca989d80ea444f427cf
PotatoJey <2227828@slu.edu.ph>
2025-01-20 15:21:20 +0800

    added login fields

--- public/index.php
+++ public/index.php
@@ -6,15 +6,44 @@
     <title>Document Reviewer Landing</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="index.css" rel="stylesheet">
+    <script src="../src\js/login.js"></script>
 </head>
 <body>
+    <div class="container">
+        <div class="header">Login</div>
+        <form action="login.php" method="post">
+            <div class="idNum"> 
+                <label form="idNum">ID Number:</label>
+                <input type="text" id="idNum" name="idNum" required>
+            </div>
+
+            <div class="pwd">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+
+            <div class="showPwd"> 
+                <input type="checkbox" onclick="showLoginPassword()">
+                <span>Show Password</span>
+            </div>
+
+            <div class="button"> 
+                <input type="submit" value="Login">
+            </div>
+
+            <button class="signup_button" type="button" onclick="window.location.href=''">Sign-Up</button>
+            <br><br>
+
+            <a href="">Forgot Password?</a>
+        </form>
+    </div>
+
     <div class="row">
         <div class="col-xs-6">
             <img src="assets/img/Samcis logo no bg.png" class="samcis_logo">
         </div>
         <div class="col-xs-6">
             <div class="mb-3">
-
             </div>
         </div>
     </div>
