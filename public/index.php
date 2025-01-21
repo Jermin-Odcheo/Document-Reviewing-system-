eecba45fa5352824180afdbaf2de4958302be933
Harvey Chua <2166734@slu.edu.ph>
2025-01-21 15:49:18 +0800

    landing page

--- public/index.php
+++ public/index.php
@@ -3,51 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document Reviewer Landing</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="index.css" rel="stylesheet">
-    <link rel="icon" type="png" href="../../img/SLU Logo.png">
-    <script src="../src\js/login.js"></script>
+    <title>None atm</title>
+    <link rel="stylesheet" href="../public/assets/styles/index.css">
 </head>
 <body>
     <div class="container">
-        <div class="header">Login</div>
-        <form action="login.php" method="post">
-            <div class="email"> 
-                <label form="idNum">ID Number:</label>
-                <input type="text" id="email" name="email" required>
-            </div>
-
-            <div class="pwd">
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
-            </div>
-
-            <div class="showPwd"> 
-                <input type="checkbox" onclick="showLoginPassword()">
-                <span>Show Password</span>
-            </div>
-
-            <div class="button"> 
-                <input type="submit" value="Login">
-            </div>
-
-            <button class="signup_button" type="button" onclick="window.location.href=''">Sign-Up</button>
-            <br><br>
-
-            <a href="">Forgot Password?</a>
-        </form>
-    </div>
-
-    <div class="row">
-        <div class="col-xs-6">
-            <img src="assets/img/Samcis logo no bg.png" class="samcis_logo">
+        <div class="left-section">
+            <img src="../public/assets/img/SLU Logo.png" alt="Logo">
         </div>
-        <div class="col-xs-6">
-            <div class="mb-3">
-            </div>
+        <div class="right-section">
+            <form class="login-form" action="../db/log-in.php" method="POST">
+            <h2 class="welcome-message">Welcome Back!</h2>
+                <input type="text" name="email" placeholder="Email" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <p class="signup-text"><a href="forgot-password.php">Forgot password?</a></p>
+                <button type="submit">Log In</button>
+      <div class="signup-container">
+              <h6>Don't have an account?</h6>
+              <a href="signup.php">Create an Account</a>
+         </div>
+            </form> 
         </div>
     </div>
-    <?php include "./assets/users_php/general/footer.php"?>
 </body>
+
+
+<script>
+    window.onload = function() {
+        const params = getQueryParams();
+        if (params.logout === 'success') {
+            alert('You have successfully logged out.');
+        } else {
+            alert('No user is logged in');
+        }
+    };
+</script>
 </html>
