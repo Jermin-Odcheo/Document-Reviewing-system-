57259727dcfe13bbf991b7841fcd01ad96132e7e
PotatoJey <2227828@slu.edu.ph>
2025-01-21 14:52:42 +0800

    registration front-end (Pampo)
    Created by Pampo

--- public/assets/users_php/general/registration.php
+++ public/assets/users_php/general/registration.php
@@ -42,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <title>Sign Up</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="styles/css/add_user.css">
+    <link rel="stylesheet" type="text/css" href="../../styles/registration.css">
     <link rel="icon" type="png" href="../../img/SLU Logo.png">
 </head>
 <body>
