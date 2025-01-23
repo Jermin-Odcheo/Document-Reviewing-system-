e604853687d3abb507709ac8bc5a6eba04819bed
CarlPascua09 <151643277+CarlPascua09@users.noreply.github.com>
2025-01-23 14:01:36 +0800

    updated database
    added additional tables (review_logs, and comments

--- config/document_reviewing_system.sql
+++ config/document_reviewing_system.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 20, 2025 at 07:43 AM
--- Server version: 8.4.3
+-- Generation Time: Jan 23, 2025 at 05:55 AM
+-- Server version: 5.7.36
 -- PHP Version: 7.4.26
 
 CREATE DATABASE IF NOT EXISTS `document_reviewing_system`;
@@ -27,12 +27,62 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE IF NOT EXISTS `comments` (
+  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `commentor_id` int(11) NOT NULL,
+  `comment_content` varchar(250) NOT NULL,
+  `resolved_status` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`comment_id`),
+  KEY `commentor_id` (`commentor_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `documents`
+--
+
+DROP TABLE IF EXISTS `documents`;
+CREATE TABLE IF NOT EXISTS `documents` (
+  `doc_id` int(11) NOT NULL AUTO_INCREMENT,
+  `uploader_id` int(11) NOT NULL,
+  `file_path` varchar(250) NOT NULL,
+  `date_uploaded` date NOT NULL,
+  `date_approved` date DEFAULT NULL,
+  `review_status` set('approved','pending','update','') NOT NULL,
+  PRIMARY KEY (`doc_id`),
+  KEY `uploader_id` (`uploader_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review_logs`
+--
+
+DROP TABLE IF EXISTS `review_logs`;
+CREATE TABLE IF NOT EXISTS `review_logs` (
+  `log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `doc_id` int(11) NOT NULL,
+  `reviewer_id` int(11) NOT NULL,
+  PRIMARY KEY (`log_id`),
+  KEY `reviewer_id` (`reviewer_id`),
+  KEY `doc_id` (`doc_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(250) NOT NULL,
   `password` varchar(25) NOT NULL,
   `first_name` varchar(250) NOT NULL,
@@ -42,24 +92,33 @@ CREATE TABLE IF NOT EXISTS `users` (
   `forgot_pass` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-COMMIT;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Table structure for table `documents`
+-- Constraints for dumped tables
 --
 
-DROP TABLE IF EXISTS `documents`;
-CREATE TABLE IF NOT EXISTS `documents` (
-  `doc_id` int NOT NULL AUTO_INCREMENT,
-  `uploader_id` int NOT NULL,
-  `file_path` varchar(250) NOT NULL,
-  `review_status` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`doc_id`),
-  KEY `uploader_id` (`uploader_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `commentor_id` FOREIGN KEY (`commentor_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `documents`
+--
+ALTER TABLE `documents`
+  ADD CONSTRAINT `documents_ibfk_1` FOREIGN KEY (`uploader_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `review_logs`
+--
+ALTER TABLE `review_logs`
+  ADD CONSTRAINT `doc_id` FOREIGN KEY (`doc_id`) REFERENCES `documents` (`doc_id`),
+  ADD CONSTRAINT `reviewer_id` FOREIGN KEY (`reviewer_id`) REFERENCES `users` (`user_id`);
 COMMIT;
 
+<<<<<<< Updated upstream
 ALTER TABLE `documents` ADD FOREIGN KEY (`uploader_id`) REFERENCES `users`(`user_id`);
 
 INSERT INTO `users` (`email`, `password`, `first_name`, `last_name`, `account_type`, `online_status`, `forgot_pass`)
@@ -69,6 +128,8 @@ INSERT INTO `documents` (`uploader_id`, `file_path`, `review_status`)
 VALUES (1, 'pdf/Sample1.pdf', 0);
 
 
+=======
+>>>>>>> Stashed changes
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
