232ac499aabfbd011534816fefff676500376932
PotatoJey <2227828@slu.edu.ph>
2025-01-24 09:18:41 +0800

    capitalize every first letter of the names

--- src/js/registration.js
+++ src/js/registration.js
@@ -1,11 +1,15 @@
+document.getElementById('first_name').addEventListener('input', function(event) {
+    this.value = this.value
+        .toLowerCase()
+        .replace(/(^\w|\s\w)/g, match => match.toUpperCase());
+});
+
+document.getElementById('last_name').addEventListener('input', function(event) {
+    this.value = this.value
+        .toLowerCase()
+        .replace(/(^\w|\s\w)/g, match => match.toUpperCase());
+});
+
 document.getElementById('email').addEventListener('input', function(event) {
-    this.value = this.value.replace(/\s/g, '');
-});
-
-document.getElementById('password').addEventListener('input', function(event) {
-    this.value = this.value.replace(/\s/g, '');
-});
-
-document.getElementById('confirm_password').addEventListener('input', function(event) {
     this.value = this.value.replace(/\s/g, '');
 });
