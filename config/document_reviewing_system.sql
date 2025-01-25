ac852b7765041bc61acd76d3db400016708f23ab
Harvey Chua <2166734@slu.edu.ph>
2025-01-25 11:04:17 +0800

    added highlight in db for comments

--- config/document_reviewing_system.sql
+++ config/document_reviewing_system.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE IF NOT EXISTS document_reviewing_system;
+USE document_reviewing_system;
+
 -- phpMyAdmin SQL Dump
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
@@ -47,7 +50,6 @@ DROP TABLE IF EXISTS `documents`;
 CREATE TABLE IF NOT EXISTS `documents` (
   `doc_id` int(11) NOT NULL AUTO_INCREMENT,
   `uploader_id` int(11) NOT NULL,
-  `file_blob` LONGBLOB NULL,
   `file_path` varchar(250) NOT NULL,
   `date_uploaded` date NOT NULL,
   `date_approved` date DEFAULT NULL,
@@ -81,6 +83,8 @@ INSERT INTO `documents` (`doc_id`, `uploader_id`, `file_path`, `date_uploaded`, 
 --
 -- Table structure for table `review_logs`
 --
+
+
 
 DROP TABLE IF EXISTS `review_logs`;
 CREATE TABLE IF NOT EXISTS `review_logs` (
@@ -142,6 +146,156 @@ ALTER TABLE `comments`
 --
 ALTER TABLE `documents`
   ADD CONSTRAINT `documents_ibfk_1` FOREIGN KEY (`uploader_id`) REFERENCES `users` (`user_id`);
+CREATE DATABASE IF NOT EXISTS document_reviewing_system;
+USE document_reviewing_system;
+
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 24, 2025 at 01:23 AM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+-- Ensure UTF8 compatibility
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `document_reviewing_system`
+--
+
+-- --------------------------------------------------------
+DROP TABLE IF EXISTS `comments`;
+DROP TABLE IF EXISTS `review_logs`;
+DROP TABLE IF EXISTS `documents`;
+DROP TABLE IF EXISTS `users`;
+
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(250) NOT NULL,
+  `password` VARCHAR(250) NOT NULL,
+  `first_name` VARCHAR(250) NOT NULL,
+  `last_name` VARCHAR(250) NOT NULL,
+  `account_type` SET('Reviewer', 'Uploader', 'Admin') NOT NULL,
+  `online_status` TINYINT(1) NOT NULL DEFAULT '0',
+  `forgot_pass` TINYINT(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `email`, `password`, `first_name`, `last_name`, `account_type`, `online_status`, `forgot_pass`) VALUES
+(1, 'admin@slu.edu.ph', '$2y$10$uxHkj9ak7t0GrgSxlLaCOuAhxDFHJ4mCiJ7Dj0.dNpdKTw6//XeNy', 'Navi', 'Gator', 'Admin', 0, 0),
+(2, '2227828@slu.edu.ph', '$2y$10$7DCRWeuTaVRtSGsXcmuWR.jZWMnehIh9U/dmK9aaQTk2LbbSVdFqe', 'Justin', 'Montemayor', 'Reviewer', 0, 0),
+(3, '2222484@slu.edu.ph', '$2y$10$eN1APMZoUmd2PRhVnWjBq.LHmxBPD0W31lGP/1wopqOUEJEjaGFhW', 'Carl', 'Pascua', 'Uploader', 0, 0),
+(4, '2202887@slu.edu.ph', '$2y$10$l8BEd//uS/MIewZovI/UHeUj3AlpbpsBit0wDd2QRejs0grZvmf7q', 'Justine', 'Lucas', 'Reviewer', 0, 0),
+(5, '2224981@slu.edu.ph', '$2y$10$htGjXhDfjNUD0kwEuYufUOXw6wYISEzUm8S16jiOGm2j/bjDtvnEy', 'Jermin', 'Odcheo', 'Uploader', 0, 0),
+(6, '2223448@slu.edu.ph', '$2y$10$AHFYI4jFwS/OtWwV6cd8muqCD4nIqaqfPKFHzGbQSeVCuFgqk3cAu', 'Dorothy', 'Lopez', 'Uploader', 0, 0),
+(7, '2166734@slu.edu.ph', '$2y$10$oEyy91FfZWQzFNtX91S5Xu3PuGTnHzVEPPD/pcnz783m8BLIe0L/y', 'Harvey', 'Chua', 'Uploader', 0, 0),
+(8, '2223258@slu.edu.ph', '$2y$10$CQxTApjgtogqvJgimzx6te6BD4QDa7cAtgiQBmQeK/9phGqJuhVg6', 'Mike', 'Mayangao', 'Reviewer', 0, 0),
+(9, '2222783@slu.edu.ph', '$2y$10$3MnhbbaxXk4Spjs5U0fEeuJPzrnsOj7eO9XJ.ujhA9OR6Gk0utymO', 'Evan', 'Garcia', 'Uploader', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `documents`
+--
+
+DROP TABLE IF EXISTS `documents`;
+CREATE TABLE IF NOT EXISTS `documents` (
+  `doc_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `uploader_id` INT(11) NOT NULL,
+  `file_path` VARCHAR(250) NOT NULL,
+  `date_uploaded` DATE NOT NULL,
+  `date_approved` DATE DEFAULT NULL,
+  `review_status` SET('approved', 'pending', 'update', '') NOT NULL,
+  PRIMARY KEY (`doc_id`),
+  KEY `uploader_id` (`uploader_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table `documents`
+INSERT INTO `documents` (`doc_id`, `uploader_id`, `file_path`, `date_uploaded`, `date_approved`, `review_status`) VALUES
+(8, 1, '../reviewer/pdf/LabActivity4-IT213.pdf', '2025-01-23', NULL, 'pending'),
+(9, 1, '../reviewer/pdf/LabActivity4-IT213.pdf', '2025-01-23', NULL, 'pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE IF NOT EXISTS `comments` (
+  `comment_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `commentor_id` INT(11) NOT NULL,
+  `comment_content` VARCHAR(250) NOT NULL,
+  `resolved_status` TINYINT(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`comment_id`),
+  KEY `commentor_id` (`commentor_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review_logs`
+--
+
+DROP TABLE IF EXISTS `review_logs`;
+CREATE TABLE IF NOT EXISTS `review_logs` (
+  `log_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `doc_id` INT(11) NOT NULL,
+  `reviewer_id` INT(11) NOT NULL,
+  PRIMARY KEY (`log_id`),
+  KEY `reviewer_id` (`reviewer_id`),
+  KEY `doc_id` (`doc_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `highlights`
+--
+
+DROP TABLE IF EXISTS `highlights`;
+CREATE TABLE IF NOT EXISTS `highlights` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `pdf_id` INT NOT NULL,
+  `highlight` TEXT NOT NULL,
+  `comment` TEXT NOT NULL,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (`pdf_id`) REFERENCES `documents` (`doc_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `commentor_id` FOREIGN KEY (`commentor_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `documents`
+--
+ALTER TABLE `documents`
+  ADD CONSTRAINT `documents_ibfk_1` FOREIGN KEY (`uploader_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `review_logs`
@@ -149,6 +303,7 @@ ALTER TABLE `documents`
 ALTER TABLE `review_logs`
   ADD CONSTRAINT `doc_id` FOREIGN KEY (`doc_id`) REFERENCES `documents` (`doc_id`),
   ADD CONSTRAINT `reviewer_id` FOREIGN KEY (`reviewer_id`) REFERENCES `users` (`user_id`);
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
